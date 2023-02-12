5ca83d12186bdff524e666968b96dc4898117c08
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-12 11:26:17 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,58 +1,52 @@
 import socket
 
 def main():
+    def commands():
+        while True:
+            command = input("Enter the command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
+            client.send(command)
+            if command == "BUY":
+                stock_symbol,user_id = ""
+                stock_amount = 0
+                price = 0
+                input("Enter stock symbol: \n")
+                input("Enter stock amount: \n")
+                input("Enter price per stock: \n")
+                input("Enter user ID: \n")
+                request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
+
+            elif command == "SELL":
+                stock_symbol,user_id = ""
+                stock_amount = 0
+                price = 0
+                input("Enter stock symbol: \n")
+                input("Enter stock amount: \n")
+                input("Enter price per stock: \n")
+                input("Enter user ID: \n")
+                request = command + " "
+
+            elif command == "LIST":
+                pass
+
+            elif command == "BALANCE":
+                balance = 0.00
+                name = ""
+                print("Balance for user " + name + ": $" + str(balance) + "\n")
+
+            elif command == "QUIT":
+                pass
+
+            elif command == "SHUTDOWN":
+                pass
+
+            else:
+                print("Error invalid input")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('0.0.0.0', 8080))
-
-    client.send("I am CLIENT\n")
-
+    client.send(commands())
     from_server = client.recv(4096)
-
     client.close()
-
     print (from_server)
-    
-
-
-    while True:
-        command = input("Enter the command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
-        if command == "BUY":
-            stock_symbol,user_id = ""
-            stock_amount = 0
-            price = 0
-            input("Enter stock symbol: \n")
-            input("Enter stock amount: \n")
-            input("Enter price per stock: \n")
-            input("Enter user ID: \n")
-            request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
-
-        elif command == "SELL":
-            stock_symbol,user_id = ""
-            stock_amount = 0
-            price = 0
-            input("Enter stock symbol: \n")
-            input("Enter stock amount: \n")
-            input("Enter price per stock: \n")
-            input("Enter user ID: \n")
-            request = command + " "
-
-        elif command == "LIST":
-            pass
-
-        elif command == "BALANCE":
-            balance = 0.00
-            name = ""
-            print("Balance for user " + name + ": $" + str(balance) + "\n")
-
-        elif command == "QUIT":
-            pass
-
-        elif command == "SHUTDOWN":
-            pass
-
-        else:
-            print("Error invalid input")
-
 
 
 if __name__ == "__main__":
