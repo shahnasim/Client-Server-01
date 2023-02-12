b24b3299f2f23104a0ff67a16eb9edd2af7800da
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-12 15:04:36 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -7,43 +7,56 @@ def main():
             command = raw_input("Enter your command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
             client.send(command)
             if command == "BUY":
-                stock_symbol,user_id = ""
+                stock_symbol = "MSFT"
+                user_id = "1234"
                 stock_amount = 0
                 price = 0
-                input("Enter stock symbol: \n")
-                input("Enter stock amount: \n")
-                input("Enter price per stock: \n")
-                input("Enter user ID: \n")
+                stock_symbol = raw_input("Enter stock symbol: \n")
+                stock_amount = raw_input("Enter stock amount: \n")
+                price = raw_input("Enter price per stock: \n")
+                user_id = raw_input("Enter user ID: \n")
                 request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
+                client.send(request)
 
             elif command == "SELL":
-                stock_symbol,user_id = ""
+                stock_symbol = "MSFT"
+                user_id = "1234"
                 stock_amount = 0
                 price = 0
-                input("Enter stock symbol: \n")
-                input("Enter stock amount: \n")
-                input("Enter price per stock: \n")
-                input("Enter user ID: \n")
-                request = command + " "
+                stock_symbol = raw_input("Enter stock symbol: \n")
+                stock_amount = raw_input("Enter stock amount: \n")
+                price = raw_input("Enter price per stock: \n")
+                user_id = raw_input("Enter user ID: \n")
+                request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
+                client.send(request)
 
             elif command == "LIST":
-                pass
+                
+                request = ""
+                client.send(request)
 
             elif command == "BALANCE":
                 balance = 0.00
                 name = ""
                 print("Balance for user " + name + ": $" + str(balance) + "\n")
+                request = ""
+                client.send(request)
 
             elif command == "QUIT":
-                pass
+
+                request = ""
+                client.send(request)
 
             elif command == "SHUTDOWN":
-                pass
+
+                request = ""
+                client.send(request)
 
             else:
                 print("Error invalid input")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('0.0.0.0', 8080))
+    SERVER_PORT = 8080
+    client.connect(('0.0.0.0', SERVER_PORT))
     client.send(commands())
     from_server = client.recv(4096)
     client.close()
@@ -52,3 +65,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
