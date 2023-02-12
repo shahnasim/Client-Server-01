a28b077cc3f50a110be8bdcd4ae50af2e2cf6cd0
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-12 14:41:02 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -7,53 +7,53 @@ def main():
     def stock():
         pass
 
+    #Processes commands
     def processBuy():
-        pass
+        print("buy")
 
     def processSell():
-        pass
+        print("sell")
 
     def processList():
-        pass
+        print("list")
 
     def processBalance():
-        pass
+        print("balance")
 
     def processQuit():
-        pass
+        print("quit")
 
     def processShutdown():
-        pass
+        print("shutdown")
     
     def option():
+    #Takes the client input and compares first two letters
         while True:
-            if data[:2].decode("uft-8") == "BU":
+            if data[:2].decode("utf-8") == "BU":
                 processBuy()
-            elif data[:2].decode("uft-8") == "SE":
+            elif data[:2].decode("utf-8") == "SE":
                 processSell()
-            elif data[:2].decode("uft-8") == "LI":
+            elif data[:2].decode("utf-8") == "LI":
                 processList()
-            elif data[:2].decode("uft-8") == "BA":
+            elif data[:2].decode("utf-8") == "BA":
                 processBalance()
-            elif data[:2].decode("uft-8") == "QU":
+            elif data[:2].decode("utf-8") == "QU":
                 processQuit()
-            elif data[:2].decode("uft-8") == "SH":
+            elif data[:2].decode("utf-8") == "SH":
                 processShutdown()
             else:
                 print("error")
 
-    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.bind(('0.0.0.0', 8080))
-    serv.listen(5)
+    #Establishing Connection
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 8080))
+    server.listen(5)
     while True:
-        conn, addr = serv.accept()
-        from_client = ''
+        conn, addr = server.accept()
         while True:
             data = conn.recv(4096)
             if not data: break
-            from_client += data
-            print (from_client)
-            conn.send("I am SERVER\n")
+            option()
         conn.close()
         print ('client disconnected')
 
