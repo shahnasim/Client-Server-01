ca74b0f0c5ef6096a017b6823d154a63cc088222
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-12 10:52:28 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,54 +1,59 @@
 import socket
-import sys
 
-SERVER_PORT = 12345
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('0.0.0.0', 8080))
 
-if len(sys.argv) != 2:
-    print("Usage: python client.py <server_ip>")
-    sys.exit(1)
+    client.send("I am CLIENT\n")
 
-server_ip = sys.argv[1]
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, SERVER_PORT))
+    from_server = client.recv(4096)
 
-command = input("Enter the command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
-while True:
-    if command == "BUY":
-        stock_symbol,user_id = ""
-        stock_amount = 0
-        price = 0
-        input("Enter stock symbol: \n")
-        input("Enter stock amount: \n")
-        input("Enter price per stock: \n")
-        input("Enter user ID: \n")
-        request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
+    client.close()
 
-    elif command == "SELL":
-        stock_symbol,user_id = ""
-        stock_amount = 0
-        price = 0
-        input("Enter stock symbol: \n")
-        input("Enter stock amount: \n")
-        input("Enter price per stock: \n")
-        input("Enter user ID: \n")
-        request = command + " "
+    print (from_server)
+    
 
-    elif command == "LIST":
-        pass
 
-    elif command == "BALANCE":
-        balance = 0.00
-        name = ""
-        print("Balance for user " + name + ": $" + str(balance) + "\n")
+    while True:
+        command = input("Enter the command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
+        if command == "BUY":
+            stock_symbol,user_id = ""
+            stock_amount = 0
+            price = 0
+            input("Enter stock symbol: \n")
+            input("Enter stock amount: \n")
+            input("Enter price per stock: \n")
+            input("Enter user ID: \n")
+            request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
 
-    elif command == "QUIT":
-        pass
+        elif command == "SELL":
+            stock_symbol,user_id = ""
+            stock_amount = 0
+            price = 0
+            input("Enter stock symbol: \n")
+            input("Enter stock amount: \n")
+            input("Enter price per stock: \n")
+            input("Enter user ID: \n")
+            request = command + " "
 
-    elif command == "SHUTDOWN":
-        pass
+        elif command == "LIST":
+            pass
 
-    else:
-        print("Error invalid input")
+        elif command == "BALANCE":
+            balance = 0.00
+            name = ""
+            print("Balance for user " + name + ": $" + str(balance) + "\n")
 
-    data = client_socket.recv(1024).decode()
-    print("Received data: ", data)
+        elif command == "QUIT":
+            pass
+
+        elif command == "SHUTDOWN":
+            pass
+
+        else:
+            print("Error invalid input")
+
+
+
+if __name__ == "__main__":
+    main()
