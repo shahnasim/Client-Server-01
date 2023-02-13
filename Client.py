fe0a3188b90c6cdbcfab3989348c80a03b92aa54
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-13 14:52:06 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,79 +3,94 @@ import sys
 import os
 
 def main():
+    # Function to define user credentials
+    def user():
 
-    def commands():
+        global userId
+        userId = server.recv()
+        if userId == None:
+            return("Error invalid UMID")
+        userId.email = ""
+        userId.fName = ""
+        userId.lName = ""
+        userId.passW = ""
+        userId.usdBalance = 0.00
+        userId.stockList = []
+
+    # Function to get stock information
+    def stock():
+        global stockSymbol
+        stockSymbol = server.recv().decode()
+        global stockName
+        stockName = server.recv().decode()
+        global stockBalance
+        
+
+    #Processes BUY command
+    def processBuy():
+        stockAmount = server.recv().decode()
+        stockPrice = server.recv().decode()
+        if userId.balance < (float(stockAmount) * float(stockPrice)):
+            print("not enough USD")
+        else:
+            userId.balance = userId.balance - (float(stockAmount) * float(stockPrice))
+
+    #Processes SELL command
+    def processSell():
+        stockAmount = server.recv().decode()
+        stockPrice = server.recv().decode()
+
+    #Processes LIST command
+    def processList():
+        for i in userId.stockList:
+            server.send(userId.stockList)
+
+    #Processes BALANCE command
+    def processBalance():
+        return("balance")
+
+    #Processes QUIT command
+    def processQuit():
+        server.close()
+
+    #Processes SHUTDOWN command
+    def processShutdown():
+        server.close()
+    
+    def option():
+    #Takes the client input and compares first two letters
         while True:
-            #Asks user what they want to do and sends command to server
-            command = raw_input("Enter your command (BUY, SELL, LIST, BALANCE, QUIT, SHUTDOWN): ")
-            client.send(command)
-
-            #Sends user info to server for BUY command
-            if command == "BUY" or "buy":
-                stock_symbol = "MSFT"
-                user_id = "1234"
-                stock_amount = 0
-                price = 0
-                stock_symbol = raw_input("Enter stock symbol: \n")
-                stock_amount = raw_input("Enter stock amount: \n")
-                price = raw_input("Enter price per stock: \n")
-                user_id = raw_input("Enter user ID: \n")
-                request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
-                client.send(request)
-
-            #Sends user info to server for SELL command
-            elif command == "SELL" or "sell":
-                stock_symbol = "MSFT"
-                user_id = "1234"
-                stock_amount = 0
-                price = 0
-                stock_symbol = raw_input("Enter stock symbol: \n")
-                stock_amount = raw_input("Enter stock amount: \n")
-                price = raw_input("Enter price per stock: \n")
-                user_id = raw_input("Enter user ID: \n")
-                request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
-                client.send(request)
-
-            #Sends user info to server for LIST command
-            elif command == "LIST" or "list":
-
-                request = command
-                client.send(request)
-
-            #Sends user info to server for BALANCE command
-            elif command == "BALANCE" or "balance":
-                balance = 0.00
-                name = ""
-                print("Balance for user " + name + ": $" + str(balance) + "\n")
-                request = command
-                client.send(request)
-
-            #Sends user info to server for QUIT command
-            elif command == "QUIT" or "quit":
-
-                request = command
-                print("200 OK")
-                client.send(request)
-
-            #Sends user info to server for SHUTDOWN command
-            elif command == "SHUTDOWN" or "shutdown":
-
-                request = command
-                client.send(request)
-
+            if data[:2].decode("utf-8") == "BU" or "bu":
+                processBuy()
+            elif data[:2].decode("utf-8") == "SE" or "se":
+                processSell()
+            elif data[:2].decode("utf-8") == "LI" or "li":
+                processList()
+            elif data[:2].decode("utf-8") == "BA" or "ba":
+                processBalance()
+            elif data[:2].decode("utf-8") == "QU" or "qu":
+                processQuit()
+            elif data[:2].decode("utf-8") == "SH" or "sh":
+                processShutdown()
             else:
-                print("400 invalid command")
+                print("error")
 
     #Establishing Connection
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER_PORT = 8080
-    client.connect(('0.0.0.0', SERVER_PORT))
-    client.send(commands())
-    from_server = client.recv(4096)
-    client.close()
-    print (from_server)
+    server.bind(('0.0.0.0', SERVER_PORT))
+    server.listen(5)
+    while True:
+        conn, addr = server.accept()
+        while True:
+            data = conn.recv(4096)
+            if not data: break
+            option()
+        conn.close()
+        print ('client disconnected')
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
+
 
