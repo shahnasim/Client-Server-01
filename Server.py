f0b6a9ee0739f988053c02123ae1dc577ab81719
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-15 19:38:27 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,102 +1,94 @@
 import socket
 
-class User:
-    def __init__(self, email, fName, lName):
-        self.email = email
-        self.fName = fName
-        self.lName = lName
-        self.balance = 0.0
+def processBuy(users, stocks, stock, price, amount, user):
+    if stock not in stocks:
+        return "404 stock not found"
+    elif users[user] < price * amount:
+        return "401 unauthorized"
+    else:
+        users[user] -= price * amount
+        stocks[stock] += amount
+        return "200 OK"
 
-class Stock:
-    def __init__(self, symbol, stockPrice, stockAmount):
-        self.symbol = symbol
-        self.stockPrice = stockPrice
-        self.stockAmount = stockAmount
-
-def processBuy(users, stocks, userId, stockPrice, stockAmount, symbol):
-    for user in users:
-        if user.email == userId:
-            totalPrice = stockPrice * stockAmount
-            if user.balance < totalPrice:
-                print("not enough balance")
-                return
-            stockExists = False
-            for stock in stocks:
-                if stock.symbol == symbol:
-                    stock.stockAmount += stockAmount
-                    stockExists = True
-                    break
-            if not stockExists:
-                stocks.append(Stock(symbol,stockPrice,stockAmount))
-            print("200 OK " + str(user.balance) + " " + str(stockAmount * stockPrice))
-            return
-    print("User not found")
-
-def processSell(users, stocks, userId, stockPrice, stockAmount, symbol):
-    for user in users:
-        if user.email == userId:
-            stockExists = False
-            for stock in stocks:
-                if stock.symbol == symbol:
-                    if stock.stockAmount < stockAmount:
-                        print("Not enough stocks")
-                        return
-                    stock.stockAmount -= stockAmount
-                    user.balance += stockPrice * stockAmount
-                    stockExists = True
-                    break
-            if not stockExists:
-                print("Stock not found")
-                return
-            print("200 OK " + str(user.balance) + " " + str(stockAmount * stockPrice))
-            return
-    print("User not found")
+def processSell(users, stocks, stock, price, amount, user):
+    if stock not in stocks:
+        return "404 stock not found"
+    elif stocks[stock] < amount:
+        return "403 forbidden"
+    else:
+        users[user] += price * amount
+        stocks[stock] -= amount
+        return "200 OK"
 
 def processList(stocks):
-    for stock in stocks:
-        print(stock.symbol, stock.stockPrice, stock.stockAmount)
+    return "\n".join([f"{stock}: {stocks[stock]}" for stock in stocks])
 
-def processBalance(users, userId, conn):
-    for user in users:
-        if user.email == userId:
-            conn.send(("Balance for user " + user.fName + " " + user.lName + ": $" + str(user.balance)).encode())
-            return
-    conn.send("User not found".encode())
+def processBalance(users, user):
+    if user not in users:
+        return "404 user not found"
+    else:
+        return f"{users[user]:.2f}"
 
 def processShutdown(server):
     server.close()
+    return "200 OK"
 
-def option(data, users, stocks, conn):
-    #Takes the client input and compares first two letters
-    if data.split()[0].upper() == "BU":
-        processBuy(users, stocks, data.split()[4], float(data.split()[3]), int(data.split()[2]), data.split()[1])
-    elif data.split()[0].upper() == "SE":
-        processSell(users, stocks, data.split()[4], float(data.split()[3]), int(data.split()[2]), data.split()[1])
-    elif data.split()[0].upper() == "LI":
-        processList(stocks)
-    elif data.split()[0].upper() == "BA":
-        processBalance(users, data.split()[1], conn)
-    elif data.split()[0].upper() == "SH":
-        processShutdown(server)
+def option(data, users, stocks, conn, server):
+    # Takes the client input and compares first two letters
+    command = data.split()[0].upper()
+    if command == "BU":
+        response = processBuy(users, stocks, data.split()[1], float(data.split()[2]), int(data.split()[3]), data.split()[4])
+    elif command == "SE":
+        response = processSell(users, stocks, data.split()[1], float(data.split()[2]), int(data.split()[3]), data.split()[4])
+    elif command == "LI":
+        response = processList(stocks)
+    elif command == "BA":
+        response = processBalance(users, data.split()[1])
+    elif command == "SH":
+        response = processShutdown(server)
     else:
-        print("Invalid command")
+        response = "400 invalid command"
+    return response
 
 def main():
-    # Define user credentials
-    users = [User("nickazzouz1520@gmail.com", "Nicholas", "Azzouz")]
-    # Define stocks
-    stocks = []
+    # Define available stocks
+    stocks = {
+        'AAPL': 100,
+        'GOOGL': 50,
+        'AMZN': 200,
+        'FB': 75,
+        'MSFT': 150
+    }
 
-    # Establishing Connection
+    # Define user data
+    users = {
+        '1': 1000.00,
+        '2': 5000.00,
+        '3': 300.00
+    }
+
+    # Create socket for server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER_PORT = 7418
     server.bind(('0.0.0.0', SERVER_PORT))
     server.listen(1)
-    conn, addr = server.accept()
-    data = conn.recv(4096)
-    option(data, users, stocks, conn)
+    print("Server started and listening on port " + str(SERVER_PORT))
+
+    while True:
+        # Wait for a client connection
+        conn, addr = server.accept()
+        print("Client connected from " + str(addr))
+
+        # Process client input
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            response = option(data.decode().strip(), users, stocks, conn, server)
+            conn.sendall(response.encode())
+
+        # Close client connection
+        conn.close()
 
 if __name__ == "__main__":
     main()
-
-
