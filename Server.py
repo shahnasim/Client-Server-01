57cc8bf12b8773c288682a6c140bba1605676341
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-16 17:17:49 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -21,13 +21,13 @@ def processSell(users, stocks, stock, price, amount, user):
         return "200 OK"
 
 def processList(stocks):
-    return "\n".join([f"{stock}: {stocks[stock]}" for stock in stocks])
+    return "\n".join(["{}: {}".format(stock, stocks[stock]) for stock in stocks])
 
 def processBalance(users, user):
     if user not in users:
         return "404 user not found"
     else:
-        return f"{users[user]:.2f}"
+        return "%.2f" % users[user]
 
 def processShutdown(server):
     server.close()
@@ -72,20 +72,20 @@ def main():
     SERVER_PORT = 7418
     server.bind(('0.0.0.0', SERVER_PORT))
     server.listen(1)
-    print("Server started and listening on port " + str(SERVER_PORT))
+    print ("Server started and listening on port " + str(SERVER_PORT))
 
     while True:
         # Wait for a client connection
         conn, addr = server.accept()
-        print("Client connected from " + str(addr))
+        print ("Client connected from " + str(addr))
 
         # Process client input
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            response = option(data.decode().strip(), users, stocks, conn, server)
-            conn.sendall(response.encode())
+            response = option(data.strip(), users, stocks, conn, server)
+            conn.sendall(response)
 
         # Close client connection
         conn.close()
