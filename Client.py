e074581f2df38f934b7d96c5b1279040bb816d91
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-02-15 19:38:13 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -10,6 +10,7 @@ def commands():
             user_id = raw_input("Enter user ID: \n")
             request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
             return request
+        
         elif command in ["SELL", "sell"]:
             stock_symbol = raw_input("Enter stock symbol: \n")
             stock_amount = raw_input("Enter stock amount: \n")
@@ -17,17 +18,22 @@ def commands():
             user_id = raw_input("Enter user ID: \n")
             request = command + " " + stock_symbol + " " + str(stock_amount) + " " + str(price) + " " + user_id + "\n"
             return request
+        
         elif command in ["LIST", "list"]:
             return command
+        
         elif command in ["BALANCE", "balance"]:
             balance = 0.00
             name = raw_input("Enter user name: \n")
             print("Balance for user " + name + ": $" + str(balance) + "\n")
             return command
+        
         elif command in ["SHUTDOWN", "shutdown"]:
             return command
+        
         elif command in ["QUIT", "quit"]:
             return command
+        
         else:
             print("400 invalid command")
 
@@ -35,7 +41,7 @@ def commands():
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER_PORT = 7418
-    server_address = '0.0.0.0'  # replace with the server IP address or hostname
+    server_address = '0.0.0.0' 
     client.connect((server_address, SERVER_PORT))
 
     while True:
@@ -45,5 +51,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
