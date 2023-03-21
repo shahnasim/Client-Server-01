53d2e04c2ac2b9287278e4c31bd3bf768c44d446
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-03-20 23:01:30 -0400

    Add files via upload

--- fix.py
+++ fix.py
@@ -1,27 +1,37 @@
+import socket
 import pandas as pd
 import openpyxl
 
-db = pd.read_excel(r'C:\Users\nicka\Desktop\PA2\database.xlsx')
-
+db = pd.read_excel('database.xlsx', sheet_name = 'main')
 db.set_index('user id', inplace=True)
+
+user_name = input("Enter User ID: \n")
+password = input("Enter pasword: \n")
+
 def login(id,pw):
     if id in db.index:
         if db.loc[id, 'password'] == pw:
             return True, db.loc[id]
     return False, None
 
-user_name = input("Enter User ID: \n")
-password = input("Enter pasword: \n")
+def stock_balance(id):
+    sheet = pd.read_excel('database.xlsx', sheet_name = id)
+    print(sheet.head())
+
+def available_stocks():
+    sheet = pd.read_excel('database.xlsx', sheet_name = 'Stocks')
+    print(sheet)
 
 success, user_info = login(user_name, password)
+
 if success:
     print(f"Logged in as user {user_info.name}")
     usd_balance = "{:.2f}".format(user_info['usd bal'])
     print(f"USD balance: ${usd_balance}")
-    print(f"Stock balance: {user_info['stock bal']}")
+    stock_balance(user_name)
 
 else:
     print("Invalid login credentials")
 
 
-
+available_stocks()
