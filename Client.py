71092e90382891b3f5378ce35f43fccc17fbc100
NickAzz50 <32042271+NickAzz50@users.noreply.github.com>
2023-03-24 12:53:20 -0400

    Add files via upload

--- Client.py
+++ Client.py
@@ -8,19 +8,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 while True:
-    action = raw_input("Enter 'login' to log in: ")
+    action = raw_input("Enter 'login' to log in or 'buy' to buy a stock: ")
     if action == 'login':
         id = raw_input("Enter your ID: ")
         pw = raw_input("Enter your password: ")
         s.sendall('{},{},{}'.format(action, id, pw).encode())
         data = s.recv(BUFFER_SIZE).decode()
         print(data)
+    elif action == 'buy':
+        user_id = raw_input("Enter your ID: ")
+        symbol = raw_input("Enter the stock symbol: ")
+        quantity = int(raw_input("Enter the quantity to buy: "))
+        s.sendall('{},{},{},{}'.format(action, user_id, symbol, quantity).encode())
+        data = s.recv(BUFFER_SIZE).decode()
+        print(data)
     else:
-        print("Invalid action. Please enter 'login'.")
+        print("Invalid action. Please enter 'login' or 'buy'.")
 s.close()
-
-
-
 
 
 
--- Server.py
+++ Server.py
@@ -17,23 +17,66 @@ def login(id, pw):
             return True, id
     return False, None
 
+def buy_stock(user_id, symbol, quantity):
+    # Load the XML file
+    tree = ET.parse('users.xml')
+    root = tree.getroot()
+
+    # Find the user with the given ID
+    user_elem = root.find("./user[@id='%s']" % user_id)
+
+    # Check if the user has enough USD balance to buy the stock
+    usd_balance = float(user_elem.find('usdBalance').text)
+    stock_price = get_stock_price(symbol)
+    if usd_balance < stock_price * quantity:
+        return False, "Insufficient balance"
+
+    # Subtract the purchase price from the user's USD balance
+    user_elem.find('usdBalance').text = str(usd_balance - stock_price * quantity)
+
+    # Check if the user already owns the stock, and if so, update the quantity
+    stock_elem = user_elem.find("stocks/stock[@symbol='%s']" % symbol)
+    if stock_elem is not None:
+        stock_elem.find('quantity').text = str(int(stock_elem.find('quantity').text) + quantity)
+    else:
+        # Otherwise, add a new stock element for the user
+        stock_elem = ET.SubElement(user_elem.find('stocks'), 'stock', {'symbol': symbol})
+        quantity_elem = ET.SubElement(stock_elem, 'quantity')
+        quantity_elem.text = str(quantity)
+
+    # Save the updated XML file
+    tree.write('users.xml')
+
+    return True, "Bought %d shares of %s for %.2f USD" % (quantity, symbol, stock_price * quantity)
+
+def get_stock_price(symbol):
+    # Replace with API call to get stock price for given symbol
+    return 10.00
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-print('Server started on {}:{}'.format(HOST, PORT))
+print ('Server started on {}:{}'.format(HOST, PORT))
 
 s.listen(1)
 
 while True:
     conn, addr = s.accept()
-    print('Connected by', addr)
-    data = conn.recv(BUFFER_SIZE).decode()
+    print ('Connected by', addr)
+    data = conn.recv(BUFFER_SIZE)
     action, id, pw = data.split(',')
     if action == 'login':
         success, user_id = login(id, pw)
         if success:
-            conn.sendall("Login successful. Welcome back, {}!".format(user_id).encode())
+            conn.sendall("Login successful. Welcome back, %s!" % user_id)
         else:
-            conn.sendall("Invalid login credentials. Please try again.".encode())
+            conn.sendall("Invalid login credentials. Please try again.")
+    elif action == 'buy':
+        symbol, quantity = id, int(pw)
+        success, message = buy_stock(user_id, symbol, quantity)
+        if success:
+            conn.sendall("Successfully bought %d shares of %s for %.2f USD" % (quantity, symbol, get_stock_price(symbol) * quantity))
+        else:
+            conn.sendall("Error: %s" % message)
     else:
-        conn.sendall("Invalid action. Please enter 'login'.".encode())
+        conn.sendall("Invalid action. Please enter 'login' or 'buy'.")
     conn.close()
